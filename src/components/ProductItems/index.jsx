c189a70b18c13fa7826adf96c111f4396bc16548
tranngoctintnt <ngoctin10a11@gmail.com>
2025-03-02 22:56:40 +0700

    final product item

--- src/components/ProductItems/index.jsx
+++ src/components/ProductItems/index.jsx
@@ -1,18 +1,45 @@
 import React from "react";
 import "../ProductItems/style.css";
 import { Link } from "react-router-dom";
-import Rating from '@mui/material/Rating';
-
+import Rating from "@mui/material/Rating";
+import { CiHeart } from "react-icons/ci";
+import { MdZoomOutMap } from "react-icons/md";
+import { Button } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
 
 const ProductItems = () => {
   return (
     <div className="productItem shadow-xl rounded-md overflow-hiden border-1 border-[rgb(0,0,0,0.1)]">
-      <div className="imgWrapper w-[100%] h-[250px] overflow-hidden rounded-md relative">
-        <img className="w-full" src="./public/nho-xanh.jpg" alt="nho" />
-       <span className="discount flex items-center absolute top-[10px] p-2 font-[500] left-[10px] z-50 te bg-[#ff5252] text-white rounded-md ">10%</span>
+      <div className="imgWrapper w-[100%] overflow-hidden rounded-md relative group">
+        <Link to="/">
+          <div className="img h-[220px] !overflow-hidden">
+            <img className="w-full" src="./public/nho-xanh.jpg" alt="nho" />
 
+            <img
+              className="w-full transition-all duration-700 absolute top-0 left-0 opacity-0 group-hover:opacity-100 group-hover:scale-105"
+              src="./public/nho-2.jpg"
+              alt="nho"
+            />
+          </div>
+        </Link>
+
+        <span className="discount flex items-center absolute top-[10px] p-2 font-[500] left-[10px] z-50 te bg-[#ff5252] text-white rounded-md ">
+          10%
+        </span>
+
+        <div className="action absolute top-[5px] right-[3px] z-50 flex items-center gap-2 flex-col w-[50px] transition-all duration-300 group-hover:top-[15px] opacity-0 group-hover:opacity-100">
+          {/* <Tooltip title="Add" placement="View Product Detailss"> */}
+
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full group !bg-white hover:!bg-[#ff5252] hover:text-white">
+            <MdZoomOutMap className="!text-black text-[18px] group-hover:text-white hover:!text-white" />
+          </Button>
+          {/* </Tooltip> */}
+
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full group !bg-white hover:!bg-[#ff5252] hover:text-white">
+            <CiHeart className="!text-black text-[18px] group-hover:text-white hover:!text-white" />
+          </Button>
+        </div>
       </div>
-
 
       <div className="info p-3 py-5 bg-[rgb(247,247,247)]">
         <h6 className=" text-[13px]">
@@ -27,15 +54,18 @@ const ProductItems = () => {
             ever since the 1500s
           </Link>
         </h3>
-        <Rating name="half-rating" defaultValue={2.5} precision={0.5} readOnly/>
+        <Rating
+          name="half-rating"
+          defaultValue={2.5}
+          precision={0.5}
+          readOnly
+        />
 
         <div className="flex items-center gap-4">
           <span className="old-price line-through text-gray-500 font-[500]">
             200.000
           </span>
-          <span className="old-price text-primary font-bold">
-            150.000
-          </span>
+          <span className="old-price text-primary font-bold">150.000</span>
         </div>
       </div>
     </div>
